15b198c086f1d3d4c6ac31108ce5ceb175cedacb
Jaroslav Kubíček <jaroslav.kubicek@gmail.com>
2012-08-18 16:46:13 +0200

    return values edited

--- IEventModel.php
+++ IEventModel.php
@@ -4,11 +4,13 @@ interface IEventModel {
     
     /**
      * zjistí, zda pro daný den existuje událost
+     * @return boolean
      */
     public function isForDate($year,$month,$day);
     
     /**
      * vrátí pole událostí pro daný den
+     * @return array
      */
     public function getForDate($year,$month,$day);
 }
