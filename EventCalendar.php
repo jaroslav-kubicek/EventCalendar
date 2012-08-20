82121c07224a3355903a295c45c62b3367b39ba9
Jaroslav Kubíček <jaroslav.kubicek@gmail.com>
2012-08-20 03:08:29 +0200

    apostrophes instead of quotation marks

--- EventCalendar.php
+++ EventCalendar.php
@@ -73,12 +73,12 @@ class EventCalendar extends UI\Control {
     public function setLanguage($language) {
         if ($language == self::ENGLISH) {
             $this->language = self::ENGLISH;
-            $this->options["bottomNavPrev"] = "Previous month";
-            $this->options["bottomNavNext"] = "Next month";
+            $this->options['bottomNavPrev'] = 'Previous month';
+            $this->options['bottomNavNext'] = 'Next month';
         } else {
             $this->language = self::CZECH;
-            $this->options["bottomNavPrev"] = "Předchozí měsíc";
-            $this->options["bottomNavNext"] = "Následující měsíc";
+            $this->options['bottomNavPrev'] = 'Předchozí měsíc';
+            $this->options['bottomNavNext'] = 'Následující měsíc';
         }
     }
 
@@ -102,7 +102,7 @@ class EventCalendar extends UI\Control {
      * @throws InvalidArgumentException 
      */
     public function setLocalNames(array $localNames) {
-        if (isset($localNames["monthNames"]) && isset($localNames["wdays"]) && count($localNames["monthNames"]) == 12 && count($localNames["wdays"]) == 7) {
+        if (isset($localNames['monthNames']) && isset($localNames['wdays']) && count($localNames['monthNames']) == 12 && count($localNames['wdays']) == 7) {
             $this->localNames = $localNames;
         } else {
             throw new InvalidArgumentException;
@@ -124,21 +124,21 @@ class EventCalendar extends UI\Control {
     }
 
     public function render() {
-        $this->template->setFile(__DIR__ . "/EventCalendar.latte");
+        $this->template->setFile(__DIR__ . '/EventCalendar.latte');
 
         // zjištění informací o aktuálním měsíci & roku
         if ($this->month == NULL || $this->year == NULL) {
             $date = getdate();
         } else {
-            $dateTime = DateTime::createFromFormat("Y-m", $this->year . "-" . $this->month);
+            $dateTime = DateTime::createFromFormat('Y-m', $this->year . '-' . $this->month);
             $date = getdate($dateTime->getTimestamp());
         }
 
         $dateInfo = array();
-        $dateInfo["year"] = $date["year"]; // aktuální rok
-        $dateInfo["month"] = $date["mon"]; // aktuální měsíc
-        $dateInfo["noOfDays"] = cal_days_in_month(CAL_GREGORIAN, $date["mon"], $date["year"]); // počet dnů v měsíci
-        $dateInfo["firstDayInMonth"] = $this->getFirstDayInMonth($date['year'], $date['mon']); // první den měsíce
+        $dateInfo['year'] = $date['year']; // aktuální rok
+        $dateInfo['month'] = $date['mon']; // aktuální měsíc
+        $dateInfo['noOfDays'] = cal_days_in_month(CAL_GREGORIAN, $date['mon'], $date['year']); // počet dnů v měsíci
+        $dateInfo['firstDayInMonth'] = $this->getFirstDayInMonth($date['year'], $date['mon']); // první den měsíce
 
         // jaké názvy měsíců použijeme?
         if ($this->localNames != NULL) {
@@ -148,8 +148,8 @@ class EventCalendar extends UI\Control {
         }
 
         $this->template->dateInfo = $dateInfo;
-        $this->template->next = $this->getNextMonth($date["year"], $date['mon']);
-        $this->template->prev = $this->getPrevMonth($date["year"], $date['mon']);
+        $this->template->next = $this->getNextMonth($date['year'], $date['mon']);
+        $this->template->prev = $this->getPrevMonth($date['year'], $date['mon']);
         $this->template->names = $names;
         $this->template->options = $this->options;
         $this->template->events = $this->events;
@@ -160,9 +160,9 @@ class EventCalendar extends UI\Control {
     /** změní aktuálně zobrazený měsíc */
     public function handleChangeMonth() {
         if ($this->presenter->isAjax()) {
-            $this->invalidateControl("ecCalendar");
+            $this->invalidateControl('ecCalendar');
         } else {
-            $this->redirect("this");
+            $this->redirect('this');
         }
     }
     
@@ -176,12 +176,12 @@ class EventCalendar extends UI\Control {
     private function getFirstDayInMonth($year, $month) {
         $day = getdate(mktime(0, 0, 0, $month, 1, $year));
         if ($this->mode == self::FIRST_SUNDAY) {
-            return $day["wday"];
+            return $day['wday'];
         } else {
-            if ($day["wday"] == 0) {
+            if ($day['wday'] == 0) {
                 return 6;
             } else {
-                return $day["wday"] - 1;
+                return $day['wday'] - 1;
             }
         }
     }
@@ -189,11 +189,11 @@ class EventCalendar extends UI\Control {
     private function getNextMonth($year, $month) {
         $next = array();
         if ($month == 12) {
-            $next["month"] = 1;
-            $next["year"] = $year + 1;
+            $next['month'] = 1;
+            $next['year'] = $year + 1;
         } else {
-            $next["month"] = $month + 1;
-            $next["year"] = $year;
+            $next['month'] = $month + 1;
+            $next['year'] = $year;
         }
         return $next;
     }
@@ -201,11 +201,11 @@ class EventCalendar extends UI\Control {
     private function getPrevMonth($year, $month) {
         $prev = array();
         if ($month == 1) {
-            $prev["month"] = 12;
-            $prev["year"] = $year - 1;
+            $prev['month'] = 12;
+            $prev['year'] = $year - 1;
         } else {
-            $prev["month"] = $month - 1;
-            $prev["year"] = $year;
+            $prev['month'] = $month - 1;
+            $prev['year'] = $year;
         }
         return $prev;
     }
@@ -219,77 +219,77 @@ class EventCalendar extends UI\Control {
         if ($this->language == self::ENGLISH) {
             if ($this->mode == self::FIRST_SUNDAY) {
                 $wdays = array(
-                    0 => "Sun",
-                    1 => "Mon",
-                    2 => "Tue",
-                    3 => "Wed",
-                    4 => "Thu",
-                    5 => "Fri",
-                    6 => "Sat"
+                    0 => 'Sun',
+                    1 => 'Mon',
+                    2 => 'Tue',
+                    3 => 'Wed',
+                    4 => 'Thu',
+                    5 => 'Fri',
+                    6 => 'Sat'
                 );
             } else {
                 $wdays = array(
-                    0 => "Mon",
-                    1 => "Tue",
-                    2 => "Wed",
-                    3 => "Thu",
-                    4 => "Fri",
-                    5 => "Sat",
-                    6 => "Sun",
+                    0 => 'Mon',
+                    1 => 'Tue',
+                    2 => 'Wed',
+                    3 => 'Thu',
+                    4 => 'Fri',
+                    5 => 'Sat',
+                    6 => 'Sun',
                 );
             }
             return array(
-                "monthNames" => array(
-                    1 => "January",
-                    2 => "February",
-                    3 => "March",
-                    4 => "April",
-                    5 => "May",
-                    6 => "June",
-                    7 => "July",
-                    8 => "August",
-                    9 => "September",
-                    10 => "October",
-                    11 => "November",
-                    12 => "December"),
-                "wdays" => $wdays);
+                'monthNames' => array(
+                    1 => 'January',
+                    2 => 'February',
+                    3 => 'March',
+                    4 => 'April',
+                    5 => 'May',
+                    6 => 'June',
+                    7 => 'July',
+                    8 => 'August',
+                    9 => 'September',
+                    10 => 'October',
+                    11 => 'November',
+                    12 => 'December'),
+                'wdays' => $wdays);
         } else {
             if ($this->mode == self::FIRST_MONDAY) {
                 $wdays = array(
-                    0 => "Po",
-                    1 => "Út",
-                    2 => "St",
-                    3 => "Čt",
-                    4 => "Pá",
-                    5 => "So",
-                    6 => "Ne"
+                    0 => 'Po',
+                    1 => 'Út',
+                    2 => 'St',
+                    3 => 'Čt',
+                    4 => 'Pá',
+                    5 => 'So',
+                    6 => 'Ne'
                 );
             } else {
                 $wdays = array(
-                    0 => "Ne",
-                    1 => "Po",
-                    2 => "Út",
-                    3 => "St",
-                    4 => "Čt",
-                    5 => "Pá",
-                    6 => "So"
+                    0 => 'Ne',
+                    1 => 'Po',
+                    2 => 'Út',
+                    3 => 'St',
+                    4 => 'Čt',
+                    5 => 'Pá',
+                    6 => 'So'
                 );
             }
             return array(
-                "monthNames" => array(
-                    1 => "Leden",
-                    2 => "Únor",
-                    3 => "Březen",
-                    4 => "Duben",
-                    5 => "Květen",
-                    6 => "Červen",
-                    7 => "Červenec",
-                    8 => "Srpen",
-                    9 => "Září",
-                    10 => "Říjen",
-                    11 => "Listopad",
-                    12 => "Prosinec"),
-                "wdays" => $wdays);
+                'monthNames' => array(
+                    1 => 'Leden',
+                    2 => 'Únor',
+                    3 => 'Březen',
+                    4 => 'Duben',
+                    5 => 'Květen',
+                    6 => 'Červen',
+                    7 => 'Červenec',
+                    8 => 'Srpen',
+                    9 => 'Září',
+                    10 => 'Říjen',
+                    11 => 'Listopad',
+                    12 => 'Prosinec'),
+                'wdays' => $wdays);
         }
     }
 
