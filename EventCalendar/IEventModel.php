c833f07839945a0f4b7a35e49e907667b453fa5b
Jaroslav Kubíček <jaroslav.kubicek@gmail.com>
2013-04-30 22:18:14 +0200

    Old version of calendar added & coding style fixed

---
+++ EventCalendar/IEventModel.php
@@ -0,0 +1,17 @@
+<?php
+
+interface IEventModel
+{
+
+    /**
+     * exists some event for that day?
+     * @return boolean
+     */
+    public function isForDate($year, $month, $day);
+
+    /**
+     * return array with events - output is NOT escaped (you can use html)
+     * @return array
+     */
+    public function getForDate($year, $month, $day);
+}
