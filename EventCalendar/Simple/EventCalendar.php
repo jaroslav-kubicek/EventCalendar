cf1c59502d740aafc6423aa672e1f928ba03b709
Jaroslav Kubíček <jaroslav.kubicek@gmail.com>
2013-05-03 02:26:56 +0200

    New type of calendar with texy support was added

---
+++ EventCalendar/Simple/EventCalendar.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace EventCalendar\Simple;
+
+use \Nette\Application\UI;
+
+class EventCalendar extends \EventCalendar\AbstractCalendar
+{
+
+    /**
+     * @var \Nette\Localization\ITranslator
+     */
+    private $translator;
+
+    public function setTranslator(\Nette\Localization\ITranslator $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    public function render()
+    {
+        $this->template->setTranslator($this->translator);
+        $this->template->wdays = $this->getWdays();
+        $this->template->monthNames = $this->getMonthNames();
+        parent::render();
+    }
+
+    protected function createTemplate($class = NULL)
+    {
+        $template = parent::createTemplate($class);
+        if (class_exists('\Texy')) {
+            $texy = new \Texy();
+            $template->registerHelper('texy', callback($texy, 'process'));
+        } else {
+            $template->registerHelper('texy', function($string) {
+                        return $string;
+                    });
+        }
+
+        return $template;
+    }
+
+    protected function getTemplateFile()
+    {
+        return __DIR__ . '/EventCalendar.latte';
+    }
+
+    protected function getWdays()
+    {
+        $wdays = array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+        if ($this->firstDay == self::FIRST_MONDAY) {
+            array_push($wdays, array_shift($wdays));
+        }
+        return $this->truncateWdays($wdays);
+    }
+
+    protected function getMonthNames()
+    {
+        $month = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+        return $month;
+    }
+
+}
--- EventCalendar/Simple/SimpleCalendar.php
+++ EventCalendar/Simple/SimpleCalendar.php
@@ -2,167 +2,33 @@
 
 namespace EventCalendar\Simple;
 
-use \Nette\Application\UI;
+use \EventCalendar\AbstractCalendar;
 use \Nette\Utils\Neon;
-use \Nette\Utils\Strings;
 
-class SimpleCalendar extends UI\Control
+class SimpleCalendar extends AbstractCalendar
 {
 
-    const FIRST_SUNDAY = 0, FIRST_MONDAY = 1;
     const LANG_EN = 'en', LANG_CZ = 'cz', LANG_SK = 'sk', LANG_DE = 'de';
 
-    /**
-     * @var int
-     * @persistent 
-     */
-    public $year = NULL;
-
-    /**
-     * @var int
-     * @persistent
-     */
-    public $month = NULL;
-    
-    /**
-     * array of callbacks fn(year, month)
-     * @var array
-     */
-    public $onDateChange;
-    
-    protected $firstDay = self::FIRST_SUNDAY;
     protected $language = self::LANG_EN;
-
-    /**
-     * @var array with options for calendar - you can change defauls by setOptions()
-     */
-    protected $options = array(
-        'showTopNav' => TRUE,
-        'showBottomNav' => TRUE,
-        'topNavPrev' => '<<',
-        'topNavNext' => '>>',
-        'bottomNavPrev' => 'Previous month',
-        'bottomNavNext' => 'Next month',
-        'wdayMaxLen' => null
-    );
-
-    /**
-     * Model which implements ICalendarEvent
-     * @var ICalendarEvent 
-     */
-    protected $events;
-
-    public function setFirstDay($day)
-    {
-        $this->firstDay = $day;
-    }
 
     public function setLanguage($lang)
     {
         $this->language = $lang;
     }
 
-    /**
-     * Changes default options
-     * 
-     * @param array $options
-     */
-    public function setOptions(array $options)
-    {
-        foreach ($options as $key => $value) {
-            $this->options[$key] = $value;
-        }
-    }
-
-    public function setEvents(\IEventModel $events)
-    {
-        $this->events = $events;
-    }
-
-    /** change current month */
-    public function handleChangeMonth()
-    {
-        $this->onDateChange($this->year, $this->month);
-        if ($this->presenter->isAjax()) {
-            $this->invalidateControl('ecCalendar');
-        } else {
-            $this->redirect('this');
-        }
-    }
-
     public function render()
     {
-        $this->template->setFile(__DIR__ . '/SimpleCalendar.latte');
-
-        // info about current month
-        if ($this->month === NULL || $this->year === NULL) {
-            $date = new \DateTime();
-        } else {
-            $date = \DateTime::createFromFormat('Y-m-d', $this->year . '-' . $this->month . '-01');
-        }
-
-        $dateInfo = array();
-        $dateInfo['year'] = $date->format('Y'); // current year
-        $dateInfo['month'] = $date->format('n'); // current month
-        $dateInfo['noOfDays'] = cal_days_in_month(CAL_GREGORIAN, $date->format('n'), $date->format('Y')); // count of days in month
-        $dateInfo['firstDayInMonth'] = $this->getFirstDayInMonth($date->format('Y'), $date->format('n')); // first day of month
-
-        $this->template->dateInfo = $dateInfo;
-        $this->template->next = $this->getNextMonth($date->format('Y'), $date->format('n'));
-        $this->template->prev = $this->getPrevMonth($date->format('Y'), $date->format('n'));
         $this->template->names = $this->getNames($this->language);
-        $this->template->options = $this->options;
-        $this->template->events = $this->events;
-
-        $this->template->render();
+        parent::render();
     }
 
-    private function getFirstDayInMonth($year, $month)
-    {
-        $day = getdate(mktime(0, 0, 0, $month, 1, $year));
-        if ($this->firstDay == self::FIRST_SUNDAY) {
-            return $day['wday'];
-        } else {
-            if ($day['wday'] == 0) {
-                return 6;
-            } else {
-                return $day['wday'] - 1;
-            }
-        }
-    }
-
-    private function getNextMonth($year, $month)
-    {
-        $next = array();
-        if ($month == 12) {
-            $next['month'] = 1;
-            $next['year'] = $year + 1;
-        } else {
-            $next['month'] = $month + 1;
-            $next['year'] = $year;
-        }
-        return $next;
-    }
-
-    private function getPrevMonth($year, $month)
-    {
-        $prev = array();
-        if ($month == 1) {
-            $prev['month'] = 12;
-            $prev['year'] = $year - 1;
-        } else {
-            $prev['month'] = $month - 1;
-            $prev['year'] = $year;
-        }
-        return $prev;
-    }
-
-    private function getNames($lang)
+    protected function getNames($lang)
     {
         $neon = Neon::decode(file_get_contents(__DIR__ . '/simpleCalData.neon'));
         if (array_key_exists($lang, $neon)) {
             $wdays = $this->truncateWdays($neon[$lang]['wdays']);
-            if ($this->firstDay) {
+            if ($this->firstDay === self::FIRST_MONDAY) {
                 array_push($wdays, array_shift($wdays));
             }
             return array('monthNames' => $neon[$lang]['monthNames'], 'wdays' => $wdays);
@@ -171,14 +37,10 @@ class SimpleCalendar extends UI\Control
         }
     }
 
-    private function truncateWdays($wdays)
+    protected function getTemplateFile()
     {
-        if ($this->options['wdayMaxLen'] > 0) {
-            foreach ($wdays as &$value) {
-                $value = Strings::substring($value, 0, $this->options['wdayMaxLen']);
-            }
-        }
-        return $wdays;
+        return __DIR__ . '/SimpleCalendar.latte';
     }
 
 }
+
